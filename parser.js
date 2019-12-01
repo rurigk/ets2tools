4247b6b1a5dc7ebf56dce477dbe580cdc17d1f29
Rurigk <rurigk@gmail.com>
2019-11-30 20:48:36 -0600

    Generate cargo, Import cargo

--- parser.js
+++ parser.js
@@ -12,6 +12,10 @@ var status = {
 	savePath: ''
 }
 
+var cargos = JSON.parse(fs.readFileSync('./Data/Cargo.json').toString());
+var cityCompany = JSON.parse(fs.readFileSync('./Data/CityCompany.json').toString());
+var trailerVariants = JSON.parse(fs.readFileSync('./Data/TrailerVariants.json').toString());
+
 const docsDir = platformFolders.getDocumentsFolder();
 const profilesPath = path.join(docsDir, 'Euro Truck Simulator 2', 'profiles');
 function GetProfiles(){
@@ -68,11 +72,11 @@ function SelectSave(savesPath)
 	.then(answers => {
 		status.save = answers.save;
 		let savePath = path.join(savesPath,answers.save, 'game.sii');
-		LoadSave(savePath)
+		LoadSaveSubroutine(savePath)
 	});
 }
 
-function LoadSave(savePath)
+function LoadSaveSubroutine(savePath)
 {
 	try{
 		status.savePath = savePath;
@@ -85,61 +89,55 @@ function LoadSave(savePath)
 					return;
 				}
 
-				SiiNunit = SiiParser.Sii.Parse(siiString);
-
-				inquirer.prompt([
-					{
-						type: 'list',
-						name: 'action',
-						message: 'What do you want to do?',
-						choices: [
-							'Change assigned trailer'
-						],
-						pageSize: 30
-					}
-				])
-				.then(answers => {
-					switch(answers.action)
-					{
-						case 'Change assigned trailer':
-							ChangeAssignedTrailerRoutine();
-							break;
-						default:
-							console.log('Unknown action')
-					}
-				});	
+				setTimeout(() => {
+					LoadSave(savePath);
+				}, 200);
 			});
 		}
 		else
 		{
-			SiiNunit = SiiParser.Sii.Parse(siiString);
-
-			inquirer.prompt([
-				{
-					type: 'list',
-					name: 'action',
-					message: 'What do you want to do?',
-					choices: [
-						'Change assigned trailer'
-					],
-					pageSize: 30
-				}
-			])
-			.then(answers => {
-				switch(answers.action)
-				{
-					case 'Change assigned trailer':
-						ChangeAssignedTrailerRoutine();
-						break;
-					default:
-						console.log('Unknown action')
-				}
-			});	
+			LoadSave(savePath);
 		}
 	}catch(e)
 	{
 		console.log('Error loading the save');
 	}
+}
+
+function LoadSave(savePath)
+{
+	var siiString = fs.readFileSync(savePath).toString();
+	SiiNunit = SiiParser.Sii.Parse(siiString);
+
+	inquirer.prompt([
+		{
+			type: 'list',
+			name: 'action',
+			message: 'What do you want to do?',
+			choices: [
+				'Change assigned trailer',
+				'Generate cargo',
+				'Import cargo'
+			],
+			pageSize: 30
+		}
+	])
+	.then(answers => {
+		switch(answers.action)
+		{
+			case 'Change assigned trailer':
+				ChangeAssignedTrailerRoutine();
+				break;
+			case 'Generate cargo':
+				GenerateCargoRoutine();
+				break;
+			case 'Import cargo':
+				ImportCargoRoutine();
+				break;
+			default:
+				console.log('Unknown action')
+		}
+	});	
 }
 
 function ChangeAssignedTrailerRoutine()
@@ -164,8 +162,98 @@ function ChangeAssignedTrailerRoutine()
 		SiiNunit.player[player]['assigned_trailer'] = new SiiParser.Token(answers.trailer);
 		var serialized = SiiParser.Sii.Serialize(SiiNunit);
 		fs.writeFileSync(status.savePath, serialized);
-		fs.writeFileSync(status.savePath+'.json', JSON.stringify(SiiNunit));
+		//fs.writeFileSync('capture.json', JSON.stringify(SiiNunit));
 	});	
+}
+
+function GenerateCargoRoutine()
+{
+	var cargoDetails = {
+		origin_city: '',
+		target_city: '',
+		origin_company: '',
+		target_company: '',
+		cargo: '',
+		trailer: ''
+	}
+	RequestRawList('Select origin city', Object.keys(cityCompany), (origin_city) => {
+		cargoDetails.origin_city = origin_city;
+		RequestRawList('Select origin company', Object.keys(cityCompany[origin_city].company), (origin_company) => {
+			cargoDetails.origin_company = origin_company;
+			RequestRawList('Select target city', Object.keys(cityCompany), (target_city) => {
+				cargoDetails.target_city = target_city;
+				RequestRawList('Select target company', Object.keys(cityCompany[target_city].company), (target_company) => {
+					cargoDetails.target_company = target_company;
+					RequestRawList('Select cargo', Object.keys(cargos), (cargo) => {
+						cargoDetails.cargo = cargo;
+						RequestRawList('Select trailer', cargos[cargo], (trailer) => {
+							cargoDetails.trailer = trailer;
+							GenerateCargo(cargoDetails);
+						})
+					})
+				})
+			})
+		})
+	})
+	// Select city and list companies
+	// Select first job related to that company anc modify job_offer_data
+}
+
+function GenerateCargo(details)
+{
+	var offerToken = SiiNunit.company[`company.volatile.${details.origin_company}.${details.origin_city}`].job_offer[0].toString();
+	SiiNunit.job_offer_data[offerToken].target = `${details.target_company}.${details.target_city}`;
+	SiiNunit.job_offer_data[offerToken].cargo = new SiiParser.Token(`cargo.${details.cargo}`);
+	SiiNunit.job_offer_data[offerToken].trailer_definition = new SiiParser.Token(details.trailer);
+	SiiNunit.job_offer_data[offerToken].trailer_variant = new SiiParser.Token(trailerVariants[details.trailer][0]);
+	var serialized = SiiParser.Sii.Serialize(SiiNunit);
+	fs.writeFileSync(status.savePath, serialized);
+	fs.writeFileSync(`./Export/Cargo/${details.origin_city}_${details.origin_company}-${details.target_city}_${details.target_company}-${details.cargo}.json`, JSON.stringify(details));
+}
+
+function ImportCargoRoutine()
+{
+	var cargos = fs.readdirSync('./Import/Cargo');
+	inquirer.prompt([
+		{
+			type: 'list',
+			name: 'cargos',
+			message: 'Select a file to import',
+			choices: cargos,
+			pageSize: 30
+		}
+	])
+	.then(answers => {
+		try{
+			let details = JSON.parse(fs.readFileSync(`./Import/Cargo/${answers.cargos}`));
+			var offerToken = SiiNunit.company[`company.volatile.${details.origin_company}.${details.origin_city}`].job_offer[0].toString();
+			SiiNunit.job_offer_data[offerToken].target = `${details.target_company}.${details.target_city}`;
+			SiiNunit.job_offer_data[offerToken].cargo = new SiiParser.Token(`cargo.${details.cargo}`);
+			SiiNunit.job_offer_data[offerToken].trailer_definition = new SiiParser.Token(details.trailer);
+			SiiNunit.job_offer_data[offerToken].trailer_variant = new SiiParser.Token(trailerVariants[details.trailer][0]);
+			var serialized = SiiParser.Sii.Serialize(SiiNunit);
+			fs.writeFileSync(status.savePath, serialized);
+		}catch(e)
+		{
+			console.log('Error loading the cargo')
+		}
+	});
+}
+
+function RequestRawList(dialog, list, callback)
+{
+	inquirer.prompt([
+		{
+			type: 'rawlist',
+			name: 'value',
+			message: dialog,
+			choices: list,
+			pageSize: 30
+		}
+	])
+	.then(answers => {
+		callback(answers.value);
+	});
 }
 
 GetProfiles();
